4cdb73981cb8da0d47833052bb466d592631cd91
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-28 21:00:04 +0200

    feat: add new users and render table

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -2,36 +2,23 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-
-        <!-- table -->
         <table>
-
-          <!-- head -->
           <thead>
             <tr>
-              <th @click="sort('name')">Name &#8595;</th>
-              <th @click="sort('age')">Age &#8595;</th>
-              <th @click="sort('gender')">Gender &#8595;</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
             </tr>
           </thead>
 
-          <!-- body -->
           <tbody>
-            <tr v-for="user in usersSort" :key="user.id">
-              <td>
-                <img :src="user.img" :alt="user.name">
-                <span>{{ user.name }}</span>
-              </td>
-              <td> {{ user.age }} </td>
-              <td> {{ user.gender }} </td>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.name }}</td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.gender }}</td>
             </tr>
           </tbody>
-
         </table>
-        <p style="text-align:center;">
-          <span> debug: sort: {{ currentSort }}, dir: {{ currentSortDir }} </span>
-          <span> page: {{ this.page.current }}, length: {{ this.page.length }} </span>
-        </p>
       </div>
     </section>
 
@@ -39,8 +26,8 @@
     <section>
       <div class="contaier">
         <div class="button-list">
-          <div class="btn btnPrimary" @click="prevPage"> &#8592; </div>
-          <div class="btn btnPrimary" @click="nextPage"> &#8594; </div>
+          <div class="btn btnPrimary">&#8592;</div>
+          <div class="btn btnPrimary">&#8594;</div>
         </div>
       </div>
     </section>
@@ -48,64 +35,20 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  data () {
-    return {
-      users: [],
-      currentSort: 'name',
-      currentSortDir: 'asc',
-      page: {
-        current: 1,
-        length: 4
-      }
-    }
+  data: () => ({
+    users: [],
+  }),
+  created() {
+    this.users = [
+      { id: 1, name: "Jack", age: 22, gender: "male" },
+      { id: 2, name: "Alex", age: 24, gender: "male" },
+    ];
   },
-  created () {
-    axios
-      .get('https://api.myjson.com/bins/rzgya')
-        .then(response => {
-          // console.log(response.data)
-          this.users = response.data
-        })
-        .catch(error => {
-          console.log(error)
-        })
-  },
-  computed: {
-    usersSort () {
-      return this.users.sort((a, b) => {
-        let mod = 1
-        if (this.currentSortDir === 'desc') mod = -1
-        if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
-        if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
-        return 0
-      }).filter((row, index) => {
-        let start = (this.page.current-1)*this.page.length
-        let end = this.page.current * this.page.length
-        if (index >= start && index < end) return true
-      })
-    }
-  },
-  methods: {
-    sort (e) {
-      if (e === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
-      }
-      this.currentSort = e
-    },
-    // Pagination
-    prevPage () {
-      if (this.page.current > 1) this.page.current-=1
-    },
-    nextPage () {
-      if ((this.page.current * this.page.length) < this.users.length) this.page.current+=1
-    }
-  }
-}
+};
 </script>
-
 
 <style lang="scss" scoped>
 img {
@@ -123,5 +66,4 @@ img {
     margin: 0 20px;
   }
 }
-
 </style>
